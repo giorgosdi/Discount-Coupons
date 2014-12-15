ee360d8eb94f6aa7b115986f6b1286be1e2aeb57
giwrgosdi <giwrgosdi@gmail.com>
2014-12-15 18:39:05 +0200

    Changes type field in users/create view from text to select. Displays errors if the required fields are not filled.

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -13,11 +13,15 @@
 			<div>
 				{{ Form::label('username', 'Username') }}
 				{{ Form::text('username') }}
+
+				{{ $errors->first('username') }}
 			</div>
 
 			<div>
 				{{ Form::label('password', 'Password') }}
 				{{ Form::password('password') }}
+
+				{{ $errors->first('password') }}
 			</div>
 
 			<div>
@@ -33,11 +37,13 @@
 			<div>
 				{{ Form::label('email', 'Email') }}
 				{{ Form::text('email') }}
+
+				{{ $errors->first('email') }}
 			</div>
 
 			<div>
 				{{ Form::label('type', 'Type') }}
-				{{ Form::text('type') }}
+				{{ Form::select('type', array('Customer', 'Professional')) }}
 			</div>
 
 			<div>
