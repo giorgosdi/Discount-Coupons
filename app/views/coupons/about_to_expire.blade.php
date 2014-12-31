a1827c9159bb3d863409cd8c5185ea4f86588ad4
giwrgosdi <giwrgosdi@gmail.com>
2014-12-31 20:05:23 +0200

    [#36] Creates pages for 'new coupons' and 'about to expire'. Removes 'active' class from the links in the navbar so we can implement it dynamically in another issue. This closes #36

---
+++ app/views/coupons/about_to_expire.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+	<h1>About to expire</h1>
+@stop
---
+++ app/views/coupons/new_coupons.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+	<h1>New coupons</h1>
+@stop
